f8d087e4f2ba9da81c945890ae2e1b8d91dc2023
ianrackson <nasirus@protonmail.com>
2021-04-14 12:38:12 -0400

    project model update in prep for project crud

--- models/Project.js
+++ models/Project.js
@@ -2,6 +2,9 @@
 const { Schema, model } = require('../db/connection');
 const marked = require('marked');
 const slugify = require('slugify');
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const dompurify = createDomPurify(new JSDOM().window);
 
 ///////////////////////////////////
 //! DEFINE OUR SCHEMA
@@ -9,19 +12,27 @@ const slugify = require('slugify');
 
 const ProjectSchema = new Schema(
     {
-        visible: { type: Boolean, default: true },
         title: { type: String, required: true },
-        codeLink: { type: String, default: 'https://github.com/irackson' },
-        liveLink: { type: String, required: true },
-        description: { type: String },
+        description: { type: String, required: true },
+        tags: { type: [String], required: true },
+        visible: { type: Boolean, default: false },
         markdown: { type: String, required: true },
         featured: { type: Boolean, default: false },
-        thumbnail: { type: String, required: true },
-        tags: [String],
+        thumbnail: {
+            type: String,
+            default:
+                'https://www.clipartmax.com/png/middle/105-1054200_summary-of-the-post-blog-icon-png.png',
+        },
+        codeLink: { type: String, default: 'https://github.com/irackson' },
+        liveLink: { type: String, required: true },
         slug: {
             type: String,
             required: true,
             unique: true,
+        },
+        sanitizedHtml: {
+            type: String,
+            required: true,
         },
     },
     { timestamps: true }
@@ -32,6 +43,10 @@ ProjectSchema.pre('validate', function (next) {
         this.slug = slugify(this.title, { lower: true, strict: true });
     }
     next();
+
+    if (this.markdown) {
+        this.sanitizedHtml = dompurify.sanitize(marked(this.markdown));
+    }
 });
 
 ///////////////////////////////////
