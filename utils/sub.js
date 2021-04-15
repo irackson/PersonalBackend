400115b0859c519d98cb48466e162dfb91b6a9e4
ianrackson <nasirus@protonmail.com>
2021-04-15 13:48:32 -0400

    uhh holy shit mail dist works...

--- utils/sub.js
+++ utils/sub.js
@@ -1,16 +1,64 @@
+// TODO
+// confirmation email
+// associate with user if logged in
+
+//! import dependencies
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+const nodemailMailgun = require('nodemailer-mailgun-transport');
+
+//! import modules;
+
+// Step 1
+const auth = {
+    auth: {
+        api_key: process.env.MAILGUN_API,
+        domain: process.env.MAILGUN_DOMAIN,
+    },
+};
+
+// Step 2
+let transporter = nodemailer.createTransport(nodemailMailgun(auth));
+
 const Sub = require('../models/Sub');
 
+const isolateEmails = (subscribers) => {
+    let emails = [];
+    for (let i = 0; i < subscribers.length; i++) {
+        if (subscribers[i].confirmation) {
+            emails.push(subscribers[i].email);
+        }
+    }
+    return emails;
+};
+
 const sendSub = async (page, post) => {
+    //! get data
     const subs = await Sub.find({}).populate({
         path: 'contentType',
     });
 
-    const sub = subs.filter((e) => e.contentType.dir === page.dir);
-    console.log(sub[0]);
+    const sub = subs.filter((e) => e.contentType.dir === page.dir)[0];
 
-    // console.log(subs);
-    // console.log(page);
-    // console.log(post);
+    //! send email
+    // Step 3
+    const recipients = isolateEmails(sub.subscribers);
+    console.log(recipients);
+    const mailOptions = {
+        from: `Ian Rackson <${process.env.MAILGUN_FROM}>`,
+        to: recipients,
+        subject: `new ${page.name}! Check out ${post.title}`,
+        text: post.description,
+    };
+
+    // Step 4
+    transporter.sendMail(mailOptions, (err, data) => {
+        if (err) {
+            console.log('Mail error: ', err);
+        } else {
+            console.log('Message sent!!');
+        }
+    });
 };
 
 module.exports = { sendSub };
