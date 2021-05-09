1b7bc4642b74eb86c9f6d50e9832062096c2c280
ianrackson <nasirus@protonmail.com>
2021-05-09 00:30:23 -0400

    cors bug

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.set('view engine', 'ejs');
 //! |  | | |__/ |__/ |___ |___ |/\| /~~\ |  \ |___
 
 app.use(
-    cors({ origin: 'https://www.ianrackson.com/', optionsSuccessStatus: 200 })
+    cors({ origin: 'https://www.ianrackson.com', optionsSuccessStatus: 200 })
 ); // prevent cors errors
 
 //! enable method override
