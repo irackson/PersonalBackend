7530261dd28af0e3f76d76c3e263ac24dec33fe7
ianrackson <nasirus@protonmail.com>
2021-05-16 17:51:08 -0400

    performance all route

--- controllers/all.js
+++ controllers/all.js
@@ -1,32 +1,19 @@
-const { buildNavbar, getNav } = require('../utils/nav');
-const { getExistingTags } = require('../utils/filter');
 const contact = require('../public/assets/Contact.json');
 const about = require('../public/assets/QA.json');
-
 const Blog = require('../models/Blog');
 const Project = require('../models/Project');
 
 const renderIndex = async (req, res) => {
-    const pages = await buildNavbar(false);
     const blogs = await Blog.find({ visible: true });
-    const blogs_filters = await getExistingTags(Blog, false);
     const projects = await Project.find({ visible: true });
-    const projects_filters = await getExistingTags(Project, false);
-
-    const featuredBlog = await Blog.findOne({ featured: true, visible: true });
-    const featuredProject = await Project.findOne({
-        featured: true,
-        visible: true,
-    });
+    const featuredBlog = blogs.find((e) => e.featured);
+    const featuredProject = projects.find((e) => e.featured);
 
     res.json({
         contact,
         about,
-        pages,
         blogs,
-        blogs_filters,
         projects,
-        projects_filters,
         featuredBlog,
         featuredProject,
     });
